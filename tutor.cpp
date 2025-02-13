b8f485ca3e5b631196c55f293cbf1d0d63a5b1ee
ARiP001 <ariffathurrahman0@gmail.com>
2025-02-14 00:49:52 +0700

    add

--- tutor.cpp
+++ tutor.cpp
@@ -2,8 +2,14 @@
 
 using namespace std;
 int main (){
-    cout << "hello world";
+    int x = 10;
+    cout << "hello world"<< endl;
 
-    cout << "github sudah berhasil";
+    cout << "github sudah berhasil\n";
+    cout << "nilai x : "<<x << endl;
+
+
+    float number = 5.2;
+    
     return 0;
 }
