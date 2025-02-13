e9a200a72891fbcbacb104342b92ac4f892eb1cf
ARiP001 <ariffathurrahman0@gmail.com>
2025-02-14 00:46:30 +0700

    Revert "update"
    This reverts commit ef0cda35a805299e0988135e40d54cd68d0e2032.

--- tutor.cpp
+++ tutor.cpp
@@ -2,10 +2,8 @@
 
 using namespace std;
 int main (){
-    int x = 10;
-    cout << "hello world"<< endl;
+    cout << "hello world";
 
-    cout << "github sudah berhasil\n";
-    cout << "nilai x : "<<x << endl;
+    cout << "github sudah berhasil";
     return 0;
 }
